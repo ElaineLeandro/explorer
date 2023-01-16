90b3983e564afa77d75668fc4be52e236c9e6d26
Elaine Leandro <elainerleandro1@gmail.com>
2023-01-16 08:27:11 -0300

    Desafio-segunda parte iniciano

--- focusTimer/js/index.js
+++ focusTimer/js/index.js
@@ -1,7 +1,6 @@
 /*
  Cada card que se encontra do lado direito, quando selecionado, vai mudar de cor e emitir um som diferente. Os sons você pode encontrar, respectivamente, nos links abaixo:
  */
-
 const tree = document.querySelector('.tree')
 const cloud = document.querySelector('.cloud')
 const popcorn = document.querySelector('.popcorn')
@@ -48,14 +47,61 @@ fire.addEventListener("click", () =>{
   }
 })
 
-/**
- * Funcionalidades esperadas dos botões:
+/* Funcionalidades esperadas dos botões:
 
-    - Play   : aciona o timer;
-    - Stop   : para o timer;
-    - +    : aumenta em mais 5 minutos o tempo do timer;
-    - -    : diminui em 5 minutos o tempo do timer.
- */
+- Play   : aciona o timer;
+- Stop   : para o timer;
+- +    : aumenta em mais 5 minutos o tempo do timer;
+- -    : diminui em 5 minutos o tempo do timer.
+*/
+
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonSum = document.querySelector('.sum')
+const buttonSub = document.querySelector('.sub')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+let timerTimerOut;
+
+function counter() {
+  setTimeout( function() {
+    let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, '0')
+
+    if( seconds <= 0) {
+        seconds = 59
+      
+      minutesDisplay.textContent =String(minutes - 1).padStart(2,'0') 
+    }
+
+    secondsDisplay.textContent = String(seconds - 1 ).padStart(2, '0')
+
+    if(minutes <= 0){
+      
+      return
+    }
+
+    counter()
+  }, 1000)
+}
+
+
+function hold (){
+  clearTimeout(timerTimerOut)
+}
+
+buttonPlay.addEventListener('click', () => {
+  counter()
+  
+
+})
+
+buttonStop.addEventListener('click', () => {
+ 
+})
 
 
 
