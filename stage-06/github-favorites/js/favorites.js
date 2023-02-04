0b8b9ba281504ba36ec1f596f6ff57e047a457d8
Elaine Leandro <elainerleandro1@gmail.com>
2023-02-03 21:38:19 -0300

    GithubUser: ajuste

--- stage-06/github-favorites/js/favorites.js
+++ stage-06/github-favorites/js/favorites.js
@@ -1,34 +1,65 @@
 //classe que vai conter a logica dos dados
 // com os dados serão estruturados
+import { GithubUser } from './GithubUser.js'
 export class Favorites{
   constructor(root){
     this.root = document.querySelector(root); 
     
     this.load()
+
+    GithubUser.search('ElaineLeandro').then(user => console.log(user))
   }
   load(){
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []   
+  }
+
+  /**
+     * async add(username){
+      console.log(username);
+      await GithubUser.search(username)
+      .then(username => {
+        this.entries.push(username)
+        this.update()
+        console.log("entries:", this.entries)
+      })
+
+  }
+   */
   
-    this.entries = [
-      {
-      login:'ElaineLeandro',
-      name: 'Elaine Leandro',
-      public_repos: '80',
-      followers: '10000'
-      },
-      {
-        login:'Deborah-Borges',
-        name: 'Debora Borges',
-        public_repos: '80',
-        followers: '9000'
-      },
-      {
-        login:'thiagoss1',
-        name: 'thiagoss1',
-        public_repos: '80',
-        followers: '9000'
-      },
-    ]
+  save(){
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+  async add(username){
+    try{
+
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      console.log(userExists)
+      if(userExists){
+        throw new Error('Usuário já cadastrado')
+      }
+
+      const user = await GithubUser.search(username)
+      if(user.login === undefined){
+        throw new Error('Usuário não encontrado!')
+
+      }
+
+      this.entries = [user,  ...this.entries]
+      this.update()
+      this.save()
+    }catch(error){
+      alert(error.message)
+    }
+  }
+
+  delete(user){
+    const filteredEntries = this.entries
+    .filter(entry => entry.login !== user.login)
     
+    this.entries = filteredEntries
+    this.update()
+    this.save()
   }
 }
 //classes que vai criar a visualização e eventos do HTML
@@ -39,18 +70,37 @@ export class FavoritesView extends Favorites{
     this.tbody = this.root.querySelector('table tbody')
 
     this.update()
+    this.onadd()
   }
 
+  onadd(){
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () =>{
+      const {value} = this.root.querySelector('.search input')
+
+      this.add(value)
+
+      console.log(value)
+    }
+  }
   update(){
     this.removeAllTr()
     this.entries.forEach(user => {
       const row = this.createRow()
       row.querySelector('.user img').src = `https://github.com/${user.login}.png`
       row.querySelector('.user img').alt =  `Imagem de ${user.name}`
+      row.querySelector('.user img').href =  `https://github.com/${user.login}`
       row.querySelector('.user p').textContent = user.name
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () =>{
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?' )
+        if(isOk){
+          this.delete(user)
+        }
+      }
      
       this.tbody.append(row)
     })
